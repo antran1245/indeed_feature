24a2157b95d88bc75dcf0a69daf690f0212193d6
antran1245 <antran1245@gmail.com>
2021-01-08 11:41:27 -0800

    remove the screenshot instructions

--- src/App.js
+++ src/App.js
@@ -4,17 +4,9 @@ import { Modal } from 'react-bootstrap';
 import './App.css';
 
 function App() {
-  const [screenOne, showOne] = useState(true);
-  const [screenTwo, showTwo] = useState(false);
-  const handleScreenOne = () => {
-    showOne(false);
-    showTwo(true);
-  }
-  const handleScreenTwo = () => showTwo(false);
+  
   return (
     <div>
-      <Modal className="screen1" show={screenOne} onHide={handleScreenOne}></Modal>
-      <Modal className="screen2" show={screenTwo} onHide={handleScreenTwo}></Modal>
       <Layout/>
     </div>
   );
--- src/database/data.js
+++ src/database/data.js
@@ -4,13 +4,13 @@ export const posting = [
         "Company": "Among Us",
         "NumberOfApp": 100,
         "Date": new Date('2020-01-31'),
-        "Body": "Looking for a handleman. Would be traveling through space. \nRequirements: \n Knowledge with electrical wire. \n Understand astronomy."
+        "Body": "Looking for a handyman. Would be traveling through space. \nRequirements: \n Knowledge with electrical wire. \n Understand astronomy."
     },
     {
         "Title": "Ship Imposter",
         "Company": "Among Us",
         "NumberOfApp": 10,
         "Date": new Date('2020-04-15'),
-        "Body": "Looking for a handleman to take over a spaceship. \nWould be needing to be ok with killing other people. \nBring your own weapons."
+        "Body": "Looking for a handyman to take over a spaceship. \nWould be needing to be ok with killing other people. \nBring your own weapons."
     }
 ];
